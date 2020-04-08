435145783231622947a55a2c5cd3121a3497fb68
尺ム丂 <gnanasreekar@gmail.com>
2020-04-08 22:54:57 +0530

    Initial commit

---
+++ app/src/main/java/com/rgs/firebasein_appmessagingdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rgs.firebasein_appmessagingdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
